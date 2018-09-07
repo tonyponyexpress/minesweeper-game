be7e94da707d8a63137e07d3c1295ebc3d87a12c
Tony Nguyen <tonynguyen979@ku.edu>
2018-09-07 11:50:58 -0500

    fixed a few errors in interface.js need to print canvas

--- js/interface.js
+++ js/interface.js
@@ -1,30 +1,33 @@
 /** Global variables, should be initialized to users input */
 
 let grid;
-let cols = 0;
-let rows = 0;
+let cols;
+let rows;
+let mines;
 let width = 20;
-let mines = 0;
-
-getInput();
+let start = false;
 
 var totalBees = 30;
 
 /** Gets user input for rows, columns, and # of mines */
 
 function getInput(){
-  cols = parseInt(document.getElementById('input2').value);
-  rows = parseInt(document.getElementById('input1').value);
-  mines = parseInt(document.getElementById('input3').value);
+
+  rows = document.getElementById("input1").value;
+  cols = document.getElementById("input2").value;
+  mines = document.getElementById("input3").value;
+  // start = true;
 }
 
 /** Creates a canvas with a 2D array according to the input*/
 function setup() {
-  createCanvas(cols*width +1, rows*width +1);
-  grid = create2DArray(cols, rows);
-  for (var c = 0; c < cols; c++) {
-    for (var r = 0; r < rows; r++) {
-      grid[c][r] = new Box(c*width, r*width, width);
+    if(start){
+    createCanvas(cols*width +1, rows*width +1);
+    grid = create2DArray(cols, rows);
+    for (let c = 0; c < cols; c++) {
+      for (let r = 0; r < rows; r++) {
+        grid[c][r] = new Box(c*width, r*width, width);
+      }
     }
   }
 }
@@ -40,8 +43,8 @@ function create2DArray(cols,rows){
 
 /** Draws the canvas on the site */
 function draw() {
-  for (var c = 0; c < cols; c++) {
-    for (var r = 0; r < rows; r++) {
+  for (let c = 0; c < cols; c++) {
+    for (let r = 0; r < rows; r++) {
       grid[c][r].show();
     }
   }
