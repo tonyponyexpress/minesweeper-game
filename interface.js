6f7e822ad4df52baf5acf164ca4c11a87d103396
robertrnickel <42808959+robertrnickel@users.noreply.github.com>
2018-09-09 23:03:59 -0500

    Added Flagging functionality

---
+++ interface.js
@@ -0,0 +1,101 @@
+/** ------------Global variables ------------ */
+
+let grid;
+let cols=0;
+let rows=0;
+let mines=0;
+let width = 30;
+let flags = 0;
+
+/** ------------ P5 interface ------------ */
+/** Creates a canvas with a 2D array according to the input*/
+function setup() {
+    let r=0;
+    let c=0;
+
+    /** Gets the dimensions from the user */
+    rows = document.getElementById("input1").value;
+    cols = document.getElementById("input2").value;
+    mines = document.getElementById("input3").value;
+
+    if (rows<2 && cols<2){
+        rows=2;
+        cols=2;
+    }
+
+    if(mines<=0){
+        mines=1;
+    }
+    if(mines>=rows*cols){
+        mines=rows*cols-1;
+    }
+    flags = mines;
+
+    createCanvas(cols*width +1, rows*width +1);
+
+    /** Creates a 2D Array with the cols and rows given*/
+    grid = create2DArray(cols, rows);
+    for (let c = 0; c < cols; c++) {
+      for (let r = 0; r < rows; r++) {
+        grid[c][r] = new Box(c*width, r*width, width);
+      }
+    }
+
+    /** Populates the grid with the amount of mines given */
+    mine_population(mines, rows, cols, grid);
+
+
+    /** Populates the count of the grid */
+    generate_playing_field(mines, rows, cols, grid);
+}
+
+
+/** Draws the canvas on the site */
+function draw() {
+  for (let c = 0; c < cols; c++) {
+    for (let r = 0; r < rows; r++) {
+      grid[c][r].show();
+    }
+  }
+}
+
+/** ------------ HELPER FUNCTIONS ------------ */
+
+/** Creates a 2D Array */
+function create2DArray(cols,rows){
+  let grid = new Array(cols);
+  for (let c=0; c<cols; c++){
+    grid[c] = new Array(rows);
+  }
+  return grid;
+}
+
+/** Works the flags when SPACEBAR is released */
+function keyReleased(){
+        if (key === ' '){
+                let x;
+                let y;
+                x = floor(mouseX/width);
+                y = floor(mouseY/width);
+                if (grid[x][y].clicked==false){
+                        if (grid[x][y].flagged==true){
+                                grid[x][y].flagged=false;
+                                flags = flags + 1;
+                        }
+                        else if (flags > 0){
+                                grid[x][y].flagged=true;
+                                flags = flags - 1;
+                        }
+                }
+        }
+}
+
+/** Test function to change the box if it is clicked */
+function mouseClicked(){
+    let x;
+    let y;
+    x = floor(mouseX/width);
+    y = floor(mouseY/width);
+    grid[x][y].clicked=true;
+    //* Need to check what is the status of the box. Right now just puts a circle on each box that is clicked */
+}
