df942b472b8cc66cc8a5e32e43f98674a187fe37
EmiliaPaz <emilia.paz@ku.edu>
2018-09-12 10:16:35 -0500

    flag counter

--- js/interface.js
+++ js/interface.js
@@ -6,6 +6,7 @@ let rows=0;
 let mines=0;
 let width = 30;
 let flags = 0;
+let firstClick = false;
 
 /** ------------ P5 interface ------------ */
 /** Creates a canvas with a 2D array according to the input*/
@@ -73,6 +74,7 @@ function create2DArray(cols,rows){
 
 /** Works the flags when SPACEBAR is released */
 function keyReleased(){
+
         if (key === ' '){
                 let x;
                 let y;
@@ -88,14 +90,21 @@ function keyReleased(){
                                 flags = flags - 1;
                         }
                 }
+                document.getElementById("flagsLeft").innerHTML = ("Remaining flags:" + flags);
+
                 if(win(cols, rows, grid, mines)){
                     window.alert("You won!");
                     location = location;
                 }
         }
+
 }
 
-/** Test function to change the box if it is clicked */
+
+/**
+ * Changes the box implementation when if its clicked
+ * @constructor
+ */
 function mouseClicked(){
     let x;
     let y;
